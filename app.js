e4c2028380487ae254c1bd7d69f98d5e7378f753
Wynny Tran <wynny.me@gmail.com>
2020-09-23 09:34:10 +0300

    Responsive jobs in Finland page is created

---
+++ app.js
@@ -0,0 +1,8 @@
+const searchBtn = document.querySelector('.search-icon--desktop');
+const modalDiv = document.querySelector('.search-modal');
+
+searchBtn.addEventListener('click', showModal);
+
+function showModal() {
+	modalDiv.classList.toggle('showModal');
+}
